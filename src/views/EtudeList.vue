d9fa410a005a1062bfa8218ffced4c06da83a343
nikopushkin <nikopushkin1@gmail.com>
2021-10-09 13:00:54 +0300

    add collections files

---
+++ src/views/EtudeList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Collection/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Collection from '@/components/collection.vue'
+
+export default {
+  name: 'EtudeList',
+  components: {
+    Collection
+  }
+}
+</script>
