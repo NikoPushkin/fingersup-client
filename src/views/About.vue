c49c2fc4d02b49136510b5321360d2dbd43fa056
nikopushkin <nikopushkin1@gmail.com>
2021-10-12 22:13:37 +0300

    ..

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="main">
+    <div class="about">
+      <div class="">
+        <h1>Welcome please to the FingersUp app</h1>
+      </div>
+      <div class="">
+        <h5>This web application provides simple and useful interface to manage etudes you play on music instruments</h5>
+      </div>
+      <div class="">
+        <h5>So you can start from
+          <router-link to="/etude/create">
+            creating
+          </router-link>
+          new etudes you want to use or look see our
+          <router-link to="/etude/collection">collection</router-link>
+        </h5>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.main {
+  display: grid;
+  width: 100%;
+  align-content: center;
+  justify-items: center;
+}
+.about {
+  display: grid;
+  width: 57%;
+  text-align: center;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="home">
+  <div class="main">
     <img alt="Vue logo" src="../assets/logo.png">
     <Base msg="Welcome to Your Vue.js App"/>
+    <div class="about">
+      <div class="">
+        <h1>Welcome please to the FingersUp app</h1>
+      </div>
+      <div class="">
+        <h5>FingersUp provides simple and useful interface to manage etudes you play on music instruments</h5>
+      </div>
+      <div class="">
+        <h5>So you can start from
+          <router-link to="/etude/create">
+            creating
+          </router-link>
+          new etudes you want to use or look see our
+          <router-link to="/etude/collection">collection</router-link>
+        </h5>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,3 +33,17 @@ export default {
   }
 }
 </script>
+<style scoped>
+.main {
+  display: grid;
+  width: 100%;
+  align-content: center;
+  justify-items: center;
+}
+.about {
+  display: grid;
+  width: 57%;
+  text-align: center;
+}
+
+</style>
